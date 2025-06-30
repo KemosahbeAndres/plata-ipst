b511058d4b2d246b65228d89a2ee16d2f6c1c07d
Andres Cubillos <andrestj1996@gmail.com>
2025-06-30 17:23:33 -0400

    configuration github pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/plata-ipst',
   plugins: [
     vue(),
     vueDevTools(),
